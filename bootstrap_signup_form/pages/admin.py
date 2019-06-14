6409a2ae69d3de14c436aa25bb81822cdf6576a7
zkan <kan@prontomarketing.com>
2019-06-14 21:13:19 +0700

    :sparkles: Register lead model to admin :bear:

--- bootstrap_signup_form/pages/admin.py
+++ bootstrap_signup_form/pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lead
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'email',
+        'country',
+        'created',
+    )
