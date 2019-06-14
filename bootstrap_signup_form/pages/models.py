cb8f07e3d85ddee0430dcb8615fcff598b3b6fb4
zkan <kan@prontomarketing.com>
2019-06-14 21:12:55 +0700

    :card_file_box: Create lead model :bear:

--- bootstrap_signup_form/pages/models.py
+++ bootstrap_signup_form/pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Lead(models.Model):
+    name = models.CharField(max_length=300)
+    email = models.EmailField(max_length=300)
+    country = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
