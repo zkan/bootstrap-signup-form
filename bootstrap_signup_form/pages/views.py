23a08b3e73a5e84c88f8971a8aad5d8a5db75758
zkan <kan@prontomarketing.com>
2019-06-14 21:13:51 +0700

    :sparkles: Submit form and save data :bear:

--- bootstrap_signup_form/pages/views.py
+++ bootstrap_signup_form/pages/views.py
@@ -1,9 +1,30 @@
+from django.contrib import messages
 from django.shortcuts import render
 from django.views import View
+
+from .models import Lead
 
 
 class IndexView(View):
     def get(self, request):
+        return render(
+            request,
+            'index.html'
+        )
+
+    def post(self, request):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        country = request.POST.get('country')
+
+        Lead.objects.create(
+            name=name,
+            email=email,
+            country=country
+        )
+
+        messages.success(request, 'Thank you for signing up!')
+
         return render(
             request,
             'index.html'
